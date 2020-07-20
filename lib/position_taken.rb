14481d936d8d630776c59c17724ea13b3599051c
Justin Mayer <justin_mayer2003@yahoo.com>
2020-07-20 20:56:20 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-  if board[index] == "x" || board[index] == "o"
+  if board[index] == "X" || board[index] == "O"
     true
   else
     board[index] == "" || board[index] == "" || board[index] == nil
